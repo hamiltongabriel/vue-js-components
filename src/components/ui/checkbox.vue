758a05fcd850a9a3d8f84b2c5750433c9d769d21
hamilton <gabrielzinho.gabriel2@gmail.com>
2018-09-17 06:13:10 -0400

    Att

---
+++ src/components/ui/checkbox.vue
@@ -0,0 +1,13 @@
+<template>
+  <label class="label">
+    <input class="input" [checked]="checked" (change)="check($event.target.checked)" type="checkbox"/>
+  </label>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
